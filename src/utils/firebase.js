199ce97ca1feb124e9f835897fc5b99631c98818
Shivam <kumaryadavshivam73@gmail.com>
2025-07-12 03:35:48 +0530

    Removing Security Issues

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,16 +1,17 @@
+// firebase.js (Create React App version)
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAmYymjQzLhQG_qg0LBfn3V_kozZ1dl7ck",
-  authDomain: "keeper-app-87dc8.firebaseapp.com",
-  projectId: "keeper-app-87dc8",
-  storageBucket: "keeper-app-87dc8.firebasestorage.app",
-  messagingSenderId: "953986595550",
-  appId: "1:953986595550:web:0099ed53f679ef7db0604f",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app); 
+export const db = getFirestore(app);
